08c1e0f6ac02afeeee5acb3ea662a547e05ac4f3
David <david_garcia.p@hotmail.com>
2017-12-02 17:45:34 -0600

    Task completed

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,42 @@
 #write your code here
+def echo(str)
+	return str
+end
+
+def shout(str)
+	return str.upcase
+end
+
+def repeat (str, n=2)
+	new_str = "#{str}"
+	(n-1).times do 
+		new_str = new_str+ " " + "#{str}"
+	end
+	new_str
+end
+
+def start_of_word(str, n)
+	return str[0..n-1]
+end
+
+def first_word(str)
+	str.split[0]
+end
+
+def titleize phrase
+  phrase.capitalize!
+  str = phrase.split
+  str.collect! do |x|
+    unless x == "and" || x == "over" || x == "the"
+      x.capitalize
+    else
+      x
+    end
+  end
+  str.join(" ")
+end
+
+
+
+			
+
