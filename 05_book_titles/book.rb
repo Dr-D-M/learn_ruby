2b56faf2c07082afb09bbfc9b150b8fbf682cb32
David <david_garcia.p@hotmail.com>
2017-12-09 15:38:28 -0600

    Task completed

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,28 @@
 class Book
 # write your code here
+#method for class Book
+
+attr_reader :title
+
+def title= (value)
+	art_conj_prep = ["the", "a", "an", "and", "in", "on", "of", "at"]
+	value = value.split(" ")
+	for i in 0...value.length do 
+
+		if art_conj_prep.include?(value[i])
+			if i == 0
+				value[i][0] = value [i][0].upcase
+			else
+
+			end
+
+		else
+			value[i][0] = value[i][0].upcase
+		end
+	end
+
+	value = value.join(" ")
+	@title = value
+end
+
 end
